ecb7557be975d62f5ae4e5f8b4f35dc4075ee72c
DuduGuima <eduardo.guima.dudu@gmail.com>
2024-06-20 09:29:53 +0200

    added heuristic to adjust matrix-vector product tolerance throughout iterations and exact version of gmres for comparing results

--- src/InexactGMRES.jl
+++ src/InexactGMRES.jl
@@ -7,6 +7,16 @@ export igmres
 
 
 """
+    rel_to_eps(res,tol)
+
+Converts the residue from iteration k-1 and overall desired tolerance into and eps we'll use to approximate the original problem's matrix A.
+
+"""
+function rel_to_eps(res::Float64, tol::Float64)
+    return min((tol/min(res,1)),1)
+end
+
+"""
     triangularsquares!(y,A,b)
 
 Solves the system Ax = b with backwards substitution, assuming A is always upper triangular.
@@ -37,8 +47,10 @@ Calculates the current_it'th iteration of the Arnoldi's Method.
 Q and H are assumed as vectors of vectors, where Q[i] stores the i-th orthonormal vector we'll be using as a basis for Km(A,b)
 and H[i] stores de i+1 first values of H's i-th column.
 """
-function my_arnoldi!(Q, H, A, current_it)
-    push!(Q, A * Q[current_it]) # heavy part should be here
+function my_arnoldi!(Q, H, A::Union{HMatrices.HTypes,HMatrices.HTriangular}, current_it, p_tol)
+    dummy_v = similar(Q[current_it])
+    mul!(dummy_v,A,Q[current_it],1,0,p_tol;threads=false)
+    push!(Q, dummy_v) # heavy part should be here
     push!(H,zeros(current_it+1))
     for j = 1:current_it
         H[current_it][j] = (Q[j]') * Q[current_it+1]
@@ -52,6 +64,25 @@ function my_arnoldi!(Q, H, A, current_it)
     #Q[current_it+1] /= H[current_it][current_it+1]
     lmul!(1/H[current_it][current_it+1],Q[current_it+1])
 end
+
+function my_arnoldi!(Q, H, A::AbstractMatrix, current_it)
+    dummy_v = similar(Q[current_it])
+    mul!(dummy_v,A,Q[current_it],1,0;threads=false)
+    push!(Q, dummy_v) # heavy part should be here
+    push!(H,zeros(current_it+1))
+    for j = 1:current_it
+        H[current_it][j] = (Q[j]') * Q[current_it+1]
+        #Q[current_it+1] -= H[current_it][j] * Q[j]
+        
+        #mul!(C,A,B,alpha,bheta) : C <- (A*B)alpha + bhetaC
+        mul!(Q[current_it+1],I,Q[j],-H[current_it][j],1)
+    end
+    H[current_it][current_it+1] = norm(Q[current_it+1])
+    
+    #Q[current_it+1] /= H[current_it][current_it+1]
+    lmul!(1/H[current_it][current_it+1],Q[current_it+1])
+end
+
 
 
 """
@@ -82,6 +113,7 @@ function my_rotation!(H, J, rhs, current_it)
     
 end
 
+
 """
     igmres(...)
 """
@@ -93,11 +125,13 @@ function igmres(A, b;maxiter=size(A, 2), restart=min(length(b), maxiter), see_r=
 
     
     x = zeros(T, size(b))#will hold answer
-    residuals = zeros(real(T), maxiter) # will hold residuals
+    #residuals = zeros(real(T), maxiter) # will hold residuals
+    residuals = Vector{Float64}()
     it = 0
     bheta = norm(b)
     m = restart
     res = bheta
+    current_perror = Float64
     
     while it < maxiter
         Q = Vector{Vector{T}}()
@@ -117,9 +151,12 @@ function igmres(A, b;maxiter=size(A, 2), restart=min(length(b), maxiter), see_r=
             if see_r
                 println("Iteration: ", it, " Current residual: ", res)
             end
+
+            ###Transformation of current residue and overall tolerance in the new error we'll use
+            current_perror = rel_to_eps(res,tol)
             ###Arnold's iteration inside GMRES to use Q,H from past iterations
             #----------------------------------------------
-            my_arnoldi!(Q, H, A, k)#no new vector is created, everything is done directly in H and Q
+            my_arnoldi!(Q, H, A, k,current_perror)#no new vector is created, everything is done directly in H and Q
             #---------------------------#
 
             ###Givens rotation
@@ -132,7 +169,8 @@ function igmres(A, b;maxiter=size(A, 2), restart=min(length(b), maxiter), see_r=
 
             #Residuals are always stored in the last element of e1
             res = norm(e1[k+1])
-            residuals[it] = res / bheta
+            #residuals[it] = res/bheta
+            push!(residuals,res/bheta)
 
             if res < tol #ta zoado esse calculo aqui tb
                 y = zero(x)
@@ -144,15 +182,92 @@ function igmres(A, b;maxiter=size(A, 2), restart=min(length(b), maxiter), see_r=
             end
         end
     end #main while loop
-    y = zero(x)
-    for n = 1:k
-        y += Q[n] * x[n]
-    end
+    # y = zero(x)
+    # for n = 1:length(x)
+    #     y += Q[n] * x[n]
+    # end
             
                 
-    println("Maximum iteration reached")
-    return y, residuals, it
+    # println("Maximum iteration reached")
+    throw("Maximum iteration reached")
 end
+
+
+###exact implementation, for comparing reasons
+
+function test_gmres(A, b;maxiter=size(A, 2), restart=min(length(b), maxiter), see_r=false, tol=sqrt(eps()))
+    #choose type to create vectors and matrices
+    TA = eltype(A)
+    Tb = eltype(b)
+    T = promote_type(TA, Tb)
+
+    
+    x = zeros(T, size(b))#will hold answer
+    residuals = Vector{Float64}()
+    it = 0
+    bheta = norm(b)
+    m = restart
+    res = bheta
+    current_perror = Float64
+    
+    while it < maxiter
+        Q = Vector{Vector{T}}()
+        H = Vector{Vector{T}}()
+        J = Vector{Any}(undef, m)#
+
+        #resduals =
+        e1 = zeros(T, m + 1, 1)
+        e1[1] = bheta
+        v = b / bheta
+        push!(Q, v)
+        for k = 1:m
+            if it >= maxiter
+                break
+            end
+            it += 1
+            if see_r
+                println("Iteration: ", it, " Current residual: ", res)
+            end
+
+            current_perror = rel_to_eps(res,tol)
+            ###Arnold's iteration inside GMRES to use Q,H from past iterations
+            #----------------------------------------------
+            my_arnoldi!(Q, H, A, k)
+            #---------------------------#
+
+            ###Givens rotation
+            #-----------------------------------
+            #Rotations on H to make it triangular
+            my_rotation!(H, J, e1, k)
+            #-------------------------------------
+
+            triangularsquares!(x, H, e1[1:k])
+
+            #Residuals are always stored in the last element of e1
+            res = norm(e1[k+1])
+            push!(residuals,res/bheta)
+
+            if res < tol #ta zoado esse calculo aqui tb
+                y = zero(x)
+                for n = 1:k
+                    y += Q[n] * x[n]
+                end
+                # println("Finished at iteration: ", it + 1, " Final residual: ", res)
+                return y, residuals, it
+            end
+        end
+    end #main while loop
+    # y = zero(x)
+    # for n = 1:length(x)
+    #     y += Q[n] * x[n]
+    # end
+            
+                
+    # println("Maximum iteration reached")
+    throw("Maximum iteration reached")
+end
+
+
 
 """
     trefethen_fast(m)
