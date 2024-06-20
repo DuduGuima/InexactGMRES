db521e7c196f4d8fce5dc5a0d2bf0169b1bc749d
DuduGuima <eduardo.guima.dudu@gmail.com>
2024-06-20 09:28:50 +0200

    adds tests to make sure Inexact GMRES is converging to the right answer

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,15 @@ using Random
 using LinearAlgebra
 using InexactGMRES
 using IterativeSolvers
+using StaticArrays
+using HMatrices
+using HMatrices: RkMatrix
+using LoopVectorization
 
 @test true == true
 
 @testset "check convergence" begin
-    m = 700
+    m = 1000
     n_iterations = 18
     A = Matrix((2.0 + 0im) * I, m, m) + 0.5 * randn(m, m) / sqrt(m)
     b = ones(ComplexF64, m)
@@ -22,7 +26,6 @@ using IterativeSolvers
 
     x_fast, residuals_fast, fast_it = igmres(A, b)
     x_slow, residuals_slow, slow_it = igmres(A_slow, b)
-    x_juliafast
 
     x_ftrue = A \ b
     x_strue = A_slow \ b
@@ -30,10 +33,43 @@ using IterativeSolvers
 
 
 
-    @test norm(A * x_fast - b) < 1e-10
-    @test norm(A_slow * x_slow - b) < 1e-10
+    @test norm(A * x_fast - b) < sqrt(eps())
+    @test norm(A_slow * x_slow - b) < sqrt(eps())
     @test fast_it < slow_it
 
+end;
+
+@testset "check HMatrix implementation" begin
+    Point2D = SVector{2,Float64}
+    m = n = 10_000
+    X = Y = [Point2D(sin(i*2π/n),cos(i*2π/n)) for i in 0:n-1]
+    nothing
+
+
+    struct LaplaceMatrix <: AbstractMatrix{Float64}
+        X::Vector{Point2D}
+        Y::Vector{Point2D}
+    end
+    
+    Base.getindex(K::LaplaceMatrix,i::Int,j::Int) = -1/2π*log(norm(K.X[i] - K.Y[j]) + 1e-10)
+    Base.size(K::LaplaceMatrix) = length(K.X), length(K.Y)
+    
+    # create the abstract matrix
+    K = LaplaceMatrix(X,Y)
+
+    Xclt = Yclt = ClusterTree(X)
+    adm = StrongAdmissibilityStd()
+    comp = PartialACA(;atol=1e-6)
+
+    H = assemble_hmatrix(K,Xclt,Yclt;adm,comp,threads=false,distributed=false)
+    T = eltype(H)
+    b = rand(T,n)
+
+    x_exact = gmres(H,b)
+    x_approx, = igmres(H,b) #no mention to tolerance will make algorithm use tol ~ 1e-8
+
+    @test norm(H*x_approx - b) < sqrt(eps())
+    @test norm(x_approx - x_exact) / norm(x_exact) < sqrt(eps())
 end;
 
 # @testset "check least squares solver" begin
